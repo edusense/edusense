92a596d9ea39c4a0bcf9c9aa7d9ed5e3c4a0df11
Dohyun Kim <dohyunk@cs.cmu.edu>
2019-12-04 20:50:55 -0500

    [storage] check secret exists before setting ENV

--- storage/entrypoint.sh
+++ storage/entrypoint.sh
@@ -10,10 +10,21 @@ adduser --shell /bin/sh -u $USER_ID --gecos "" --quiet --disabled-login --no-cre
 chown -R edusense:edusense /app
 
 # Read secrets and then deescalate the previleges
-SSL_CERT="$(cat /run/secrets/ssl_cert)"
-SSL_CERT_PRIVATE_KEY="$(cat /run/secrets/ssl_cert_private_key)"
-APP_USERNAME="$(cat /run/secrets/app_username)"
-APP_PASSWORD="$(cat /run/secrets/app_password)"
+if [ -f /run/secrets/ssl_cert ]; then
+  SSL_CERT="$(cat /run/secrets/ssl_cert)"
+fi
+
+if [ -f /run/secrets/ssl_cert_private_key ]; then
+  SSL_CERT_PRIVATE_KEY="$(cat /run/secrets/ssl_cert_private_key)"
+fi
+
+if [ -f /run/secrets/app_username ]; then
+  APP_USERNAME="$(cat /run/secrets/app_username)"
+fi
+
+if [ -f /run/secrets/app_password ]; then
+  APP_PASSWORD="$(cat /run/secrets/app_password)"
+fi
 
 exec env SSL_CERT="$SSL_CERT" \
   env SSL_CERT_PRIVATE_KEY="$SSL_CERT_PRIVATE_KEY" \
