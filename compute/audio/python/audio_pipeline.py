49928427244e78586738f83677dd8d5adb96416f
Pranav Dheer <pranav@edusense-dev-1.andrew.cmu.edu>
2020-06-29 21:54:01 -0400

    making audio version controlled

--- compute/audio/python/audio_pipeline.py
+++ compute/audio/python/audio_pipeline.py
@@ -2,49 +2,24 @@
 # Use of this source code is governed by BSD 3-clause license.
 
 from vggish_input import waveform_to_examples
-from keras.models import load_model
 import vggish_params
-import tensorflow as tf
 import numpy as np
 from scipy.io import wavfile
-import pyaudio
 import time
 import subprocess
-import soundtransfer
 import os
 import re
 import sys
-import inspect
 import argparse
 from joblib import dump, load
 import time
 from datetime import date,datetime,timedelta
 import json
-import struct
 import requests
 import base64
 import get_time as gt
-import cv2
-import threading
 
 frame_number = 0
-context = soundtransfer.everything
-trained_model = "models/example_model.hdf5"
-
-labels = soundtransfer.labels
-context = soundtransfer.everything
-
-print("Using deep learning model: %s" % (trained_model))
-model = load_model(trained_model)
-graph = tf.compat.v1.get_default_graph()
-
-label = dict()
-for k in range(len(context)):
-    label[k] = context[k]
-
-
-def avg(l):
-    return sum(l) / float(len(l))
 
 def sampling_rate(video):
     specs=subprocess.Popen(['ffmpeg','-i',video,'-debug_ts'],stderr= subprocess.PIPE).stderr.read()
@@ -58,14 +33,7 @@ def sampling_rate(video):
     return sampling
 
 
-# Variables
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-CHUNK = RATE  # 1024
 float_dtype = '>f4'
-
-clf = load('models/svm_speech.joblib')
 
 ##############################
 # Parase Args
@@ -199,40 +167,6 @@ try:
         amp1 = max(abs(np_wav1))
         amp2 = max(abs(np_wav2))
         ##############################
-        """    
-        speech1 = 0
-        speech2 = 0
-        X = []
-        X.append([max(np_wav1), max(np_wav2), max(np_wav1)/float(max(np_wav2))])
-        prediction_teacher_vs_student = clf.predict(X)[0]
-        predictions = []
-        with graph.as_default():
-            if x1.shape[0] != 0:
-                x1 = x1.reshape(len(x1), 96, 64, 1)
-                pred = model.predict(x1)
-                predictions.append(pred)
-            for prediction in predictions:
-                n_items = prediction.shape[1]
-                for k in range(n_items):
-                    if label[k].capitalize() == 'Speech':
-                        speech1 = prediction[0, k]
-        predictions = []
-        with graph.as_default():
-            if x2.shape[0] != 0:
-                x2 = x2.reshape(len(x2), 96, 64, 1)
-                pred = model.predict(x2)
-                predictions.append(pred)
-            for prediction in predictions:
-                n_items = prediction.shape[1]
-                for k in range(n_items):
-                    if label[k].capitalize() == 'Speech':
-                        speech2 = prediction[0, k]
-
-        mel_feats1 = list(
-            map(lambda x: list(map(lambda y: round(y, 2), x)), mel_feats1.tolist()[0]))
-        mel_feats2 = list(
-            map(lambda x: list(map(lambda y: round(y, 2), x)), mel_feats2.tolist()[0]))
-        """
         ## set the time stamps
         if not realtime:
            timestamp1=f"{date1}T{str(time1)}Z"
